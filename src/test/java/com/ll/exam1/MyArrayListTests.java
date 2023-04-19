28b946ce005c44317629285e7332360dd25e02ac
Park0720 <ppmm98@naver.com>
2023-04-19 15:14:25 +0900

    data's size increase

--- src/test/java/com/ll/exam1/MyArrayListTests.java
+++ src/test/java/com/ll/exam1/MyArrayListTests.java
@@ -34,4 +34,15 @@ class MyArrayListTests {
         assertThat(list.get(0)).isEqualTo("사과");
         assertThat(list.get(1)).isEqualTo("포도");
     }
+    @Test
+    @DisplayName("data's size increase Automatically")
+    void t04() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+        list.add("당근");
+
+        assertThat(list.size()).isEqualTo(3);
+    }
 }
