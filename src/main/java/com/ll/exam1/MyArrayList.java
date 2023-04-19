a78050ca2cb3fffee4de17e1248e447cbe9e4e76
Park0720 <ppmm98@naver.com>
2023-04-19 14:26:57 +0900

    testcase : testsize()

---
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -0,0 +1,8 @@
+package com.ll.exam1;
+
+
+public class MyArrayList<T> {
+    public int size(){
+        return 0;
+    }
+}
---
+++ src/test/java/com/ll/exam1/MyArrayListTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyArrayListTests {
+    @Test
+    void testSize() {
+        MyArrayList<String> list = new MyArrayList<>();
+        assertEquals(0, list.size());
+    }
+}
